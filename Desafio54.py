e99743d1915bad39d683ec1619557e9809bc2f7f
Monique Aguiar <monikeagn@gmail.com>
2024-02-19 14:32:01 -0300

    novo exercicio

--- Desafio54.py
+++ Desafio54.py
@@ -2,7 +2,7 @@ from datetime import date
 atual = date.today().year
 totmaior = 0
 totmenor = 0
-for c in range (1, 8):
+for c in range (1, 4): #aparecem 3 pessoas. Se quero 5 pessoas, coloco número 6, sempre um número a mais do que quero.
     num = int(input('Em que ano a {}º pessoa nasceu? '.format(c)))
     idade = atual - num
     if idade >= 21:
@@ -11,6 +11,7 @@ for c in range (1, 8):
         totmenor += 1
 print ('Ao todo tivemos {} pessoas maiores de idade'.format(totmaior))
 print('E também tivemos {} pessoas menores de idade'.format(totmenor))
+
 
 
 
---
+++ Desafio55.py
@@ -0,0 +1,22 @@
+#A solução abaixo fiz antes da resposta, deu certo, mas não era o resultado que o exercio pedia.
+##for c in range (1, 6):
+#    peso = float(input('Qual seu peso? '.format(c)))
+#    if peso >= 50:
+#       print('Vocês tá acima do peso!')
+#    else:
+#        print('Você está no peso ideal! Parabéns!')
+
+maior = 0
+menor = 0
+for p in range (1, 6):
+    peso = float(input('Peso da {}º pessoa: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso verificado foi de {}Kg'.format(maior))
+print('O menor peso verificado foi de {}Kg'.format(menor))

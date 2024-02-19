37784c9e01f65e4b6c92d0941cb08ffa69aaa10e
Monique Aguiar <monikeagn@gmail.com>
2024-02-19 15:04:42 -0300

    finalizado

--- Desafio55.py
+++ Desafio55.py
@@ -1,11 +1,15 @@
 #A solução abaixo fiz antes da resposta, deu certo, mas não era o resultado que o exercio pedia.
 ##for c in range (1, 6):
 #    peso = float(input('Qual seu peso? '.format(c)))
-#    if peso >= 50:
+#    if peso >= 70:
 #       print('Vocês tá acima do peso!')
 #    else:
 #        print('Você está no peso ideal! Parabéns!')
 
+print('='*25)
+print('→ Coloque seu peso abaixo:')
+print('='*25)
+\n
 maior = 0
 menor = 0
 for p in range (1, 6):
@@ -18,5 +22,6 @@ for p in range (1, 6):
             maior = peso
         if peso < menor:
             menor = peso
-print('O maior peso verificado foi de {}Kg'.format(maior))
-print('O menor peso verificado foi de {}Kg'.format(menor))
+
+print('→ O maior peso verificado foi de {}Kg'.format(maior))
+print('→ O menor peso verificado foi de {}Kg'.format(menor))
